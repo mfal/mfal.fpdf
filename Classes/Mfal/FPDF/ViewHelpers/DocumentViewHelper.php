cd7d7c455b7642373d4023887ce1b37edb8f4aa2
Marco Falkenberg <marco@mfal.de>
2014-07-09 01:24:24 +0200

    Initialize FPDF instance earlier
    Due to the early creation, the view helper arguments could
    be passed directly to the constructor arguments. So there is
    no need to adjust FPDF in an unsafe way via properties.

--- Classes/Mfal/FPDF/ViewHelpers/DocumentViewHelper.php
+++ Classes/Mfal/FPDF/ViewHelpers/DocumentViewHelper.php
@@ -7,38 +7,36 @@ use TYPO3\Flow\Http\Response;
 
 class DocumentViewHelper extends AbstractFPDFViewHelper {
 
-	protected $scaleFactor = array();
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('defaultPageOrientation', 'string', '', FALSE, 'P');
+		$this->registerArgument('unit', 'string', '', FALSE, 'mm');
+		$this->registerArgument('defaultSize', 'string', '', FALSE, 'A4');
+		$this->registerArgument('name', 'string', '', FALSE, 'document.pdf');
+		$this->registerArgument('destination', 'string', '', FALSE, 'I');
+	}
 
-	public function initialize() {
-		parent::initialize();
-
-		$this->scaleFactor = array('pt' => 1,
-			'mm' => (72 / 25.4),
-			'cm' => (72 / 2.54),
-			'in' => 72
+	protected function callRenderMethod() {
+		$fpdf = new \fpdf\FPDF(
+			$this->arguments['defaultPageOrientation'],
+			$this->arguments['unit'],
+			$this->arguments['defaultSize']
 		);
 
-		$fpdf = new \fpdf\FPDF();
 		$this->templateVariableContainer->add($this->variableName, $fpdf);
+
+		return parent::callRenderMethod();
 	}
 
 	/**
-	 * @param string $defaultPageOrientation
-	 * @param string $unit
-	 * @param string $defaultSize
-	 * @param string $name
-	 * @param string $destination
 	 * @throws \TYPO3\Flow\Mvc\Exception\StopActionException
 	 */
-	public function render($defaultPageOrientation = 'P', $unit = 'mm', $defaultSize = 'A4', $name = 'document.pdf', $destination = 'I') {
-		$this->fpdf()->k = $this->scaleFactor[$unit];
-		$this->fpdf()->DefOrientation = $defaultPageOrientation;
-		$this->fpdf()->DefPageSize = $defaultSize;
+	public function render() {
 		$this->setDefaultFont();
 
 		$this->renderChildren();
 
-		// Todo: Or like this?
+		// Todo: Or better like this?
 		//$this->fpdf()->Output(NULL, $destination);
 		//exit();
 
@@ -48,18 +46,18 @@ class DocumentViewHelper extends AbstractFPDFViewHelper {
 				'Cache-Control' => 'private, max-age=0, must-revalidate',
 				'Pragma' => 'public',
 			);
-			if ($destination === 'D') {
+			if ($this->arguments['destination'] === 'D') {
 				$header['Content-Type'] = 'application/x-download';
-				$header['Content-Disposition'] = 'attachment; filename="' . $name . '"';
+				$header['Content-Disposition'] = 'attachment; filename="' . $this->arguments['name'] . '"';
 			} else {
 				$header['Content-Type'] = 'application/pdf';
-				$header['Content-Disposition'] = 'inline; filename="' . $name . '"';
+				$header['Content-Disposition'] = 'inline; filename="' . $this->arguments['name'] . '"';
 			}
 			$response->setHeaders(new Headers($header));
 			$response->setContent($this->fpdf()->Output(NULL, 'S'));
 		} else if ($response instanceof CliResponse) {
-			$this->fpdf()->Output($name, 'S');
-			$response->setContent('Saved file to ' . $name);
+			$this->fpdf()->Output($this->arguments['name'], 'S');
+			$response->setContent('Saved file to ' . $this->arguments['name']);
 		}
 		throw new \TYPO3\Flow\Mvc\Exception\StopActionException();
 	}
